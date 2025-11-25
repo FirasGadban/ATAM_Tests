66272d2569c624a85988c1621425074407586f55
SaraBader1 <174009110+SaraBader1@users.noreply.github.com>
2025-11-25 20:12:34 +0200

    Generated 50 tests files for ex3

---
+++ EX1/tests/ex3_test47.asm
@@ -0,0 +1,44 @@
+
+.section .data
+# הקלט לתרגיל
+str:
+    .string "MAAooo"
+
+# המקום שבו הקוד שלך צריך לכתוב את התוצאה
+enc:
+    .zero 200      # מקצה 200 בתים מאופסים לכתיבה
+
+# התוצאה הצפויה (לצורך בדיקה בלבד)
+expected:
+    .string "1M2A3o"
+
+.section .text
+# --- בדיקת התוצאה (strcmp) ---
+check_result:
+    # הכנת מצביעים להשוואה
+    movq $enc, %rdi        # המחרוזת שלך
+    movq $expected, %rsi   # המחרוזת הנכונה
+
+compare_loop:
+    movb (%rdi), %al       # טען תו מהפלט שלך
+    movb (%rsi), %bl       # טען תו מהצפי
+    
+    cmpb %al, %bl          # השוואה
+    jne error              # אם שונים - שגיאה
+    
+    cmpb $0, %al           # האם הגענו לסוף המחרוזת (NULL)?
+    je success             # אם כן (וגם השני היה 0 כי עברנו את ה-cmp), הצלחה
+    
+    incq %rdi              # קדם מצביע
+    incq %rsi              # קדם מצביע
+    jmp compare_loop
+
+error:
+    movq $60, %rax
+    movq $1, %rdi
+    syscall
+
+success:
+    movq $60, %rax
+    xorq %rdi, %rdi
+    syscall
---
+++ EX1/tests/ex3_test6.asm
@@ -0,0 +1,44 @@
+
+.section .data
+# הקלט לתרגיל
+str:
+    .string "XXXXXXXXXXXqqqqqqqqqqqYYaaaaaaaaaIIIIIIIIIIIUUUUUUUUUUUUUOOOOOOOKKKKKKK"
+
+# המקום שבו הקוד שלך צריך לכתוב את התוצאה
+enc:
+    .zero 200      # מקצה 200 בתים מאופסים לכתיבה
+
+# התוצאה הצפויה (לצורך בדיקה בלבד)
+expected:
+    .string "11X11q2Y9a11I13U7O7K"
+
+.section .text
+# --- בדיקת התוצאה (strcmp) ---
+check_result:
+    # הכנת מצביעים להשוואה
+    movq $enc, %rdi        # המחרוזת שלך
+    movq $expected, %rsi   # המחרוזת הנכונה
+
+compare_loop:
+    movb (%rdi), %al       # טען תו מהפלט שלך
+    movb (%rsi), %bl       # טען תו מהצפי
+    
+    cmpb %al, %bl          # השוואה
+    jne error              # אם שונים - שגיאה
+    
+    cmpb $0, %al           # האם הגענו לסוף המחרוזת (NULL)?
+    je success             # אם כן (וגם השני היה 0 כי עברנו את ה-cmp), הצלחה
+    
+    incq %rdi              # קדם מצביע
+    incq %rsi              # קדם מצביע
+    jmp compare_loop
+
+error:
+    movq $60, %rax
+    movq $1, %rdi
+    syscall
+
+success:
+    movq $60, %rax
+    xorq %rdi, %rdi
+    syscall
